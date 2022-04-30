32f01035cd26d16abf24f0562aeba91f4e354440
Frederico Reckziegel <freddalsoglio@gmail.com>
2022-04-30 17:46:59 -0300

    add signal conversion cases

---
+++ optimization4/case1_poor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]) {
+    clock_t time;
+    int x = 3, y = 4, z = 5, s = 0; 
+    int trials = 10, loops = 1000000000;
+    double total_time_taken = 0.0;
+    
+    for (int j = 0; j < trials; j++)
+    {
+        time = clock();
+        for (int i = 0; i < loops; i++)
+        {
+            s = -(x-y*z);
+        }
+        time = clock() - time;
+        total_time_taken += ((double)time)/CLOCKS_PER_SEC;
+    }
+    printf("Case 1 | Poor | Time: %lfs\n", total_time_taken/trials);
+    
+    return 0;
+}
