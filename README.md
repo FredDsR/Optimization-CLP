c192ece34e03a2cda35d27e7375f3fb8928af2fc
Frederico Reckziegel <freddalsoglio@gmail.com>
2022-04-30 16:04:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Optimization-CLP
---
+++ case1/case2_fast.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]) {
+    clock_t time;
+    int x = 3, y = 0, trials = 5, loops = 1000000;
+    double total_time_taken = 0.0;
+    
+    for (int j = 0; j < trials; j++)
+    {
+        time = clock();
+        for (int i = 0; i < loops; i++)
+        {
+            y = x*x;
+        }
+        time = clock() - time;
+        total_time_taken += ((double)time)/CLOCKS_PER_SEC;
+    }
+    printf("Case 2 | Fast | Time: %lfs\n", total_time_taken/trials);
+    
+    return 0;
+}
