64e3902e0d65e692154cdf0ab47c7a64980a890e
Frederico Reckziegel <freddalsoglio@gmail.com>
2022-04-30 17:29:00 -0300

    add loop const expressions cases

---
+++ optimization3/case1_fast.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]) {
+    clock_t time;
+    int x = 3, y = 4, z = 5, aux = 0; 
+    int trials = 10, loops = 1000000;
+    double total_time_taken = 0.0;
+    
+    for (int j = 0; j < trials; j++)
+    {
+        time = clock();
+        for (int i = 0; i < loops; i++)
+        {
+            aux = y * z;
+            for (int k = 0; k < 3000; k++)
+            {
+                x = x + aux;
+                
+            }
+        }
+        time = clock() - time;
+        total_time_taken += ((double)time)/CLOCKS_PER_SEC;
+    }
+    printf("Case 1 | Poor | Time: %lfs\n", total_time_taken/trials);
+    
+    return 0;
+}
